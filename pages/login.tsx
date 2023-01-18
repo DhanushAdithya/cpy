e06f084fc69e55426ffe5f458e726e8f27100b74
Dhanush Adithya <s.dhanushadithya@gmail.com>
2023-01-18 22:00:29 +0530

    feat(app): Add login and signup pages with auth context
    - Add login and signup pages to handle user authentication
    - Use auth context to handle user's state and provide login and signup functionality
    - Update the routes to include the new pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,66 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Nav, L, ThemeToggle } from "@components";
+import { useAuth } from "@context";
+import { useRouter } from "next/router";
 
 const Login: NextPage = () => {
-    return (
-        <form>
-            <input type="email" />
-            <input type="password" />
-            <input type="submit" />
-        </form>
-    )
-}
+	const { user, logIn } = useAuth();
+	const router = useRouter();
+	const [form, setForm] = useState({
+		email: "",
+		password: "",
+	});
+
+	useEffect(() => {
+		if (user) router.push("/c");
+	}, []);
+
+	const onSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+
+		const { error, message } = await logIn(form.email, form.password);
+		if (error) console.log(message);
+		router.push("/c");
+	};
+
+	const updateField = (evt: ChangeEvent<HTMLInputElement>): void => {
+		setForm({
+			...form,
+			[evt.target.name]: evt.target.value,
+		});
+	};
+
+	return (
+		<>
+			<Nav form />
+			<L>
+				<form className="primary-form" onSubmit={onSubmit}>
+					<input
+						name="email"
+						type="email"
+						placeholder="johndoe@example.com"
+						value={form.email}
+						onChange={updateField}
+					/>
+					<input
+						name="password"
+						type="password"
+						placeholder="********"
+						value={form.password}
+						onChange={updateField}
+					/>
+					<button
+						type="submit"
+						className="p-2 px-9 border-2 text-lg text-white hover:bg-white hover:text-black ease-in duration-100 font-bold"
+					>
+						Log In
+					</button>
+				</form>
+				<ThemeToggle />
+			</L>
+		</>
+	);
+};
 
 export default Login;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,14 +1,85 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Nav, ThemeToggle } from "@components";
+import { useAuth } from "@context";
+import { useRouter } from "next/router";
 
 const Signup: NextPage = () => {
-    return (
-        <form>
-            <input type="name" />
-            <input type="email" />
-            <input type="password" />
-            <input type="submit" />
-        </form>
-    )
-}
+	const { user, signUp } = useAuth();
+	const router = useRouter();
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		password: "",
+		uname: "",
+	});
+
+	useEffect(() => {
+		if (user) router.push("/c");
+	}, []);
+
+	const onSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+
+		const { error, message } = await signUp(
+			form.name,
+			form.uname,
+			form.email,
+			form.password
+		);
+		if (error) console.log(message);
+		router.push("/c");
+	};
+
+	const updateField = (evt: ChangeEvent<HTMLInputElement>): void => {
+		setForm({
+			...form,
+			[evt.target.name]: evt.target.value,
+		});
+	};
+
+	return (
+		<>
+			<Nav form />
+			<form className="primary-form" onSubmit={onSubmit}>
+				<input
+					type="name"
+					name="name"
+					placeholder="Name"
+					value={form.name}
+					onChange={updateField}
+				/>
+				<input
+					type="name"
+					name="uname"
+					placeholder="Username"
+					value={form.uname}
+					onChange={updateField}
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+					value={form.email}
+					onChange={updateField}
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="*****"
+					value={form.password}
+					onChange={updateField}
+				/>
+				<button
+					type="submit"
+					className="p-2 px-9 border-2 text-lg text-white hover:bg-white hover:text-black ease-in duration-100 font-bold"
+				>
+					Sign Up
+				</button>
+				<ThemeToggle />
+			</form>
+		</>
+	);
+};
 
 export default Signup;
