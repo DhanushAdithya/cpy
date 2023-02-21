388daa12b8c4f91ca0dd6705c47e5d349289937d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2023-02-21 20:43:03 +0530

    feat: add necessary packages and pre-commit hook

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,28 @@ module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		fontFamily: {
+			display: ['"DM Serif Display", serif'],
 			sans: ['"DM Sans"', "sans-serif"],
 			dmserif: ['"DM Serif Display", serif'],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				dark: "#1A0634",
+				"secondary-dark": "#2B0D52",
+				"sidebar-dark": "#1C073A",
+				"card-dark": "#210A3F",
+				"font-dark": "#ffffff",
+				light: "#E5FAE6",
+				secondary: "#F5F5F5",
+				sidebar: "#ffffff",
+				card: "#f5f5f5",
+				"font-light": "#000000",
+			},
+			backgroundImage: {
+				"blob-light": "url('../assests/bg-light.svg')",
+				"blob-dark": "url('../assests/bg.svg')",
+			},
+		},
 	},
 	plugins: [],
 };
