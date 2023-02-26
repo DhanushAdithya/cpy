c5e575d428434a7c2100ad1fa7f605803bcebdc8
Dhanush Adithya <s.dhanushadithya@gmail.com>
2023-02-26 19:15:11 +0530

    feat(tag): added edit and delete feature
    - modified TagAdd component such that now it can both add and update a
      tag
    - Header component will now show an edit and a delete button on tags
      page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,63 @@
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 import { useState } from "react";
-import { Plus } from "react-feather";
+import { Edit, Plus, Trash2 } from "react-feather";
+import { trpc } from "~/utils/trpc";
 import CpyAdd from "./CpyAdd";
 import Portal from "./Portal";
+import TagAdd from "./TagAdd";
 
 const Header = ({ title }: { title?: string }) => {
 	const [show, setShow] = useState<boolean>(false);
+	const [tagShow, setTagShow] = useState<boolean>(false);
+	const router = useRouter();
+	const { mutate } = trpc.cpy.deleteTag.useMutation();
 
 	return (
 		<>
-			<div className="sticky top-5 z-30">
+			<div className="sticky top-5 z-30 dark:bg-dark bg-light">
 				<div className="flex justify-between">
 					<h1 className="text-3xl m-0 p-0 font-dmserif">
 						{title ?? "All"}
 					</h1>
-					<motion.button
-						whileTap={{ scale: 0.9 }}
-						className="w-10 h-10 flex items-center dark:bg-secondary-dark shadow-lg justify-center bg-gray-300 rounded-full"
-						onClick={() => setShow(!show)}
-					>
-						<Plus />
-					</motion.button>
+					<div className="flex space-x-3">
+						{router.pathname.includes("/tags") && (
+							<>
+								<motion.button
+									whileTap={{ scale: 0.9 }}
+									className="w-10 h-10 flex items-center dark:bg-secondary-dark shadow-lg justify-center bg-gray-300 rounded-full"
+									onClick={() => setTagShow(!tagShow)}
+								>
+									<Edit />
+								</motion.button>
+								<motion.button
+									whileTap={{ scale: 0.9 }}
+									className="w-10 h-10 flex items-center dark:bg-secondary-dark shadow-lg justify-center bg-gray-300 rounded-full"
+									onClick={() => mutate(title as string)}
+								>
+									<Trash2 />
+								</motion.button>
+							</>
+						)}
+						<motion.button
+							whileTap={{ scale: 0.9 }}
+							className="w-10 h-10 flex items-center dark:bg-secondary-dark shadow-lg justify-center bg-gray-300 rounded-full"
+							onClick={() => setShow(!show)}
+						>
+							<Plus />
+						</motion.button>
+					</div>
 				</div>
 				<hr className="border-0 my-4 mt-3 h-0.5 dark:bg-white bg-black w-full" />
 			</div>
 			{show && (
 				<Portal show={show} setShow={setShow}>
 					<CpyAdd />
+				</Portal>
+			)}
+			{tagShow && (
+				<Portal show={tagShow} setShow={setTagShow}>
+					<TagAdd update name={title} />
 				</Portal>
 			)}
 		</>
--- src/components/TagAdd.tsx
+++ src/components/TagAdd.tsx
@@ -4,29 +4,37 @@ import { useCpy } from "~/context/CpyContext";
 import { trpc } from "~/utils/trpc";
 import Toast from "./Toast";
 
-const TagAdd = () => {
+type TagProps = {
+	name?: string;
+	update?: boolean;
+};
+
+const TagAdd = ({ name, update = false }: TagProps) => {
 	const [loading, setLoading] = useState(false);
 	const [showToast, setToast] = useState(false);
-	const [form, setForm] = useState({
-		name: "",
-	});
-	const { mutate } = trpc.cpy.addTag.useMutation();
+	const [tagName, setTagName] = useState(name ?? "");
+	const { mutate } = update
+		? trpc.cpy.updateTag.useMutation()
+		: trpc.cpy.addTag.useMutation();
 
 	const { tagsRefetch } = useCpy();
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setLoading(true);
-		mutate(form, {
-			onSuccess() {
-				tagsRefetch();
-				setToast(true);
-				setTimeout(() => {
-					setToast(false);
-				}, 2000);
-				setLoading(false);
-			},
-		});
+		mutate(
+			{ name, tagName },
+			{
+				onSuccess() {
+					tagsRefetch();
+					setToast(true);
+					setTimeout(() => {
+						setToast(false);
+					}, 2000);
+					setLoading(false);
+				},
+			}
+		);
 	};
 
 	return (
@@ -35,14 +43,12 @@ const TagAdd = () => {
 				type="text"
 				placeholder="Tag Name"
 				name="name"
-				value={form.name}
+				value={tagName}
 				minLength={1}
 				maxLength={12}
 				pattern="[A-z0-9_-]{1,12}"
-				className="invalid:border-red-900 border-2 outline-none border-transparent"
-				onChange={e =>
-					setForm({ ...form, [e.target.name]: e.target.value })
-				}
+				className="invalid:border-re-900 border-2 outline-none border-transparent"
+				onChange={e => setTagName(e.target.value)}
 			/>
 			<motion.button
 				whileTap={{ scale: 0.95 }}
@@ -53,10 +59,10 @@ const TagAdd = () => {
 				{loading ? (
 					<>
 						<div className="w-4 h-4 animate-spin border-4 border-t-white rounded-full border-transparent mr-4"></div>
-						Adding ...
+						<>{update ? "Updating ..." : "Adding ..."}</>
 					</>
 				) : (
-					<>Add Tag</>
+					<>{update ? "Update Tag" : "Add Tag"}</>
 				)}
 			</motion.button>
 			<Toast show={showToast} msg={"Tag Added!"} />
