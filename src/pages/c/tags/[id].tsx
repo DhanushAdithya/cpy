aeebdddf3d45aa2263fcaa999d8b7fecc460a90d
Dhanush Adithya <s.dhanushadithya@gmail.com>
2023-02-26 19:13:12 +0530

    fix(tags): added fix for unknown tags in route
    - now the users cannot go to a tag that is nonexistent even by modifying the
      URL directly

--- src/pages/c/tags/[id].tsx
+++ src/pages/c/tags/[id].tsx
@@ -1,23 +1,37 @@
 import { useRouter } from "next/router";
 import { Layout } from "~/components";
 import CpyCard from "~/components/CpyCard";
+import { useCpy } from "~/context/CpyContext";
 import { trpc } from "~/utils/trpc";
 
 const Tag = () => {
-	const { query } = useRouter();
-	const { id } = query as { id: string };
-	const { data, isLoading } = trpc.cpy.getTag.useQuery({ id });
+	const router = useRouter();
+	const { id } = router.query as { id: string };
+	const { tagsList, tagsLoading } = useCpy();
+
+	const { data, isLoading } = trpc.cpy.getTag.useQuery({ name: id });
+
+	if (tagsLoading) return null;
+
+	if (!tagsLoading && !tagsList.tags.map(tag => tag.name).includes(id)) {
+		router.push("/c");
+	}
 
 	return (
 		<Layout title={id}>
 			{isLoading
 				? null
-				: data?.list.map(cpy => (
+				: data &&
+				  data.list.map(cpy => (
 						<CpyCard
 							id={cpy.id}
 							name={cpy.name}
 							key={cpy.id}
 							content={cpy.content}
+							isProtected={cpy.isprotected}
+							isArchived={cpy.isarchived}
+							isPublic={cpy.ispublic}
+							tag={cpy.tags[0]}
 						/>
 				  ))}
 		</Layout>
